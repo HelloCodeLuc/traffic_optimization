1265563ce7f193669d9b7bb0d8d672e3c9eeb95a
HelloCodeLuc <Lucasjchan01@gmail.com>
2023-10-03 22:58:26 -0400

    fix multiple problems starting with calling a windows version of randomTrips.py

--- random_trip_generator.py
+++ random_trip_generator.py
@@ -1,17 +1,42 @@
+
+import sys
 import os
 import subprocess
 import traci
 import random
 import time
+import shutil
 
 def generate_random_trips(trip_file, max_steps, seed):
     # Run randomtrips.py to generate random trips and save them to a file
-    cmd = f"python randomtrips.py -n keeleandmajmack.net.xml -r {trip_file} -e {max_steps} --random -s {seed}"
-    subprocess.call(cmd, shell=True)
+    print ("DEBUG 0")
+    #cmd = f"C:/Users/chuny/Desktop/lucas/Python%20Projects/traffic_optimization/randomTrips.py -n OSM_RandomTrips/keeleandmajmack.net.xml -r {trip_file} -e {max_steps} --random -s {seed} -o output/trips.trips.xml"
+    randomTrips = r'"C:\Program Files (x86)\Eclipse\Sumo\tools\randomTrips.py"'
+    cmd = f"python {randomTrips} -n OSM_RandomTrips/keeleandmajmack.net.xml -r {trip_file} -e {max_steps} --random -s {seed}"
 
-def run_sumo():
-    # Launch SUMO and load the generated random trips
-    sumo_cmd = ["sumo-gui", "-c", "keeleandmajmack.sumocfg"]
+    print (f"DEBUG 1 : randomTrips.py command : {cmd}")
+    subprocess.call(cmd, shell=True)
+    print ("DEBUG 2")
+
+def generate_sumo_config(config_file, route_files):
+    # Generate the SUMO configuration file with the given template
+    config_template = f"""<configuration>
+    <input>
+        <net-file value="OSM_RandomTrips/keeleandmajmack.net.xml"/>
+        <route-files value="{route_files}"/>
+    </input>
+    <time>
+        <begin value="0"/>
+        <end value="2000"/>
+    </time>
+</configuration>"""
+
+    with open(config_file, 'w') as f:
+        f.write(config_template)
+
+def run_sumo(config_file):
+    # Launch SUMO with GUI using the generated configuration file
+    sumo_cmd = ["sumo", "-c", config_file]
     traci.start(sumo_cmd)
 
     step = 0
@@ -22,19 +47,44 @@ def run_sumo():
 
     traci.close()
 
+
 if __name__ == "__main__":
+
+    output_folder = "output"
+
+    try:
+        # Use shutil.rmtree() to remove the directory and its contents
+        shutil.rmtree(output_folder)
+        print(f'Deleted directory: {output_folder}')
+    except Exception as e:
+        print(f"Error: {e}")
+
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+
     num_runs = 1  # Change this to the number of times you want to run the simulation
-    max_steps = 2000  # Change this to the desired number of simulation steps
+    max_steps = 5
+    #max_steps = 2000  # Change this to the desired number of simulation steps
 
     for run in range(num_runs):
         random_seed = random.randint(1, 10000)  # Use a different random seed for each run
-        trip_file = f"random_trips_{random_seed}.xml"  # Generate a unique trip file name for each run
-
+        trip_file = os.path.join(output_folder, f"random_trips_{random_seed}.xml")  # Generate a unique trip file name for each run
+        print (f"DEBUG : trip_file = {trip_file}")
         # Generate random trips
         generate_random_trips(trip_file, max_steps, random_seed)
+        print ("DEBUG 3")
+        #sys.exit(0)
+        # Generate SUMO configuration file and update the route-files value
+        config_file = os.path.join(output_folder, f"sumo_config_{random_seed}.sumocfg")
+        generate_sumo_config(config_file, route_files=trip_file)
 
-        # Run the SUMO simulation
-        run_sumo()
+        # Set working directory to the output folder for the SUMO simulation
+        os.chdir(output_folder)
 
-        # Clean up generated trip file
+        # Run the SUMO simulation using the generated configuration file
+        #run_sumo(config_file)
+
+        # Clean up generated files
+        print (f"DEBUG : trip_file = {trip_file}")
         os.remove(trip_file)
+        os.remove(config_file)
